831b8c5d1c15a933c1ce204b63f405b3406acafa
Marcus Lin <marcuslin0724@gmail.com>
2025-08-05 15:22:56 +0800

    Set up rspec
    - Add `.rspec`, `spec/rails_helper.rb` and `spec/spec_helper.rb`

---
+++ spec/rails_helper.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+# Add FactoryBot and Shoulda Matchers
+require 'factory_bot_rails'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
+RSpec.configure do |config|
+  # Use transactional fixtures (default - no DatabaseCleaner needed)
+  config.use_transactional_fixtures = true
+
+  # FactoryBot methods (create, build, etc.) without FactoryBot prefix
+  config.include FactoryBot::Syntax::Methods
+
+  # Infer spec type from file location
+  config.infer_spec_type_from_file_location!
+
+  # Filter Rails backtrace
+  config.filter_rails_from_backtrace!
+end
+
+# Shoulda Matchers configuration
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
