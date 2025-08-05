e729847b9d059f34726be871a7dc143bdbd325d3
Marcus Lin <marcuslin0724@gmail.com>
2025-08-05 14:59:51 +0800

    Renamed followers_follows relation in follows
    - The original `followers_follows` seems to be a little bit
    confusing, renamed it to `followed_by` instead.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
   
-  # Following relationships
+  # Who this user follows
   has_many :follows, foreign_key: :follower_id, dependent: :destroy
   has_many :followees, through: :follows, source: :followee
   
-  # Follower relationships  
-  has_many :followers_follows, class_name: 'Follow', foreign_key: :followee_id, dependent: :destroy
-  has_many :followers, through: :followers_follows, source: :follower
+  # Who follows this user
+  has_many :followed_by, class_name: 'Follow', foreign_key: :followee_id, dependent: :destroy
+  has_many :followers, through: :followed_by, source: :follower
 end
