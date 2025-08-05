1dd05697cc60abb4b661070c6222c84388b7ad0f
Marcus Lin <marcuslin0724@gmail.com>
2025-08-05 14:50:19 +0800

    Add relation for user and follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :sleep_records, dependent: :destroy
+  
+  # Following relationships
+  has_many :follows, foreign_key: :follower_id, dependent: :destroy
+  has_many :followees, through: :follows, source: :followee
+  
+  # Follower relationships  
+  has_many :followers_follows, class_name: 'Follow', foreign_key: :followee_id, dependent: :destroy
+  has_many :followers, through: :followers_follows, source: :follower
 end
