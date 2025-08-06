4f514391c648d9d16863ab7a3425f4e53d20f040
Marcus Lin <marcuslin0724@gmail.com>
2025-08-06 11:31:44 +0800

    Finish sleep_records/clock_in_interaction
    - Add `sleep_records/clock_in_interaction`
    - Finish adding buisness logic and validations in `clock_in_interaction`

---
+++ app/interactions/sleep_records/clock_in_interaction.rb
@@ -0,0 +1,20 @@
+class SleepRecords::ClockInInteraction < ActiveInteraction::Base
+  object :user
+
+  validate :sleeping_record_exists
+
+  def execute
+    user.sleep_records.create!(
+      clock_in_time: Time.now,
+      status: :sleeping
+    )
+  end
+
+  private
+
+  def sleeping_record_exists
+    if user.sleep_records.sleeping.exists?
+      errors.add(:base, I18n.t("interactions.sleep_records.clock_in.already_sleeping"))  
+    end
+  end
+end
